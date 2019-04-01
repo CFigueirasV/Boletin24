c57371866ccc1eae37db5c74268cebef98971d7c
cfigueirasvalverde <cfigueirasvalverde@nautilus26.danielcastelao.org>
2019-04-01 12:16:17 +0200

    boletin24_paleta

---
+++ Boletin24_paleta/src/boletin24_paleta/Boletin24_paleta.java
@@ -0,0 +1,14 @@
+package boletin24_paleta;
+
+public class Boletin24_paleta {
+
+    
+    public static void main(String[] args) {
+        
+        Ventana ven = new Ventana();
+        ven.setVisible(true);
+        
+        
+    }
+    
+}
